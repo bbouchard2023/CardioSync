8b2bb044434c2c316a0a801efcf2d90c9cd0b067
bbouchard2023 <bbouchard2023@my.fit.edu>
2026-03-24 16:29:39 -0400

    Program now connects pump via serial, runs input speed and revolutions, and stops after a specified delay

--- ctlr.adb
+++ ctlr.adb
@@ -25,13 +25,14 @@ procedure ctlr is
 	
 	
 	
-	subtype Message is Stream_Element_Array (1 .. 40);
+	subtype Message is Stream_Element_Array (1 .. 11);
 	
 	Enquiry			: constant Stream_Element_Array := ( -- Sends an enquiry (checks if the pump is connected)
 		1 => 16#02#, -- STX
 		2 => 16#05#, -- ENQ
 		3 => 16#0D# -- CR
 	);
+	
 	
 	Numeration   	: constant Stream_Element_Array  := ( -- Sets the pump's number (P01)
 		1 => 16#02#, -- STX
@@ -69,23 +70,23 @@ procedure ctlr is
 		2 => 16#50#, -- P
 		3 => 16#30#, -- 0
 		4 => 16#31#, -- 1
-		5 => 16#69#, -- I
+		5 => 16#49#, -- I
 		6 => 16#0D# -- CR
 	);
 	
-	StopSignal		: constant Stream_Element_Array := (
+	StopSignal		: constant Stream_Element_Array := ( -- Tells the pump to stop
 		1 => 16#02#, -- STX
 		2 => 16#50#, -- P
 		3 => 16#30#, -- 0
 		4 => 16#31#, -- 1
-		5 => 16#68#, -- H
+		5 => 16#48#, -- H
 		6 => 16#0D# -- CR
 	);
-	Buffer 	: Message;
+	
+	Buffer	: Message; -- Sets up the buffer for reading the StatusCheck
+	Last	: Stream_Element_Offset; -- Sets the endpoint for the Buffer
 
-	Last   : Stream_Element_Offset;
-
-	S_Port : constant Natural := 1;
+	S_Port	: constant Natural := 1; -- defines the serial port name ("COM<S_Port>") 
 
 begin
 
@@ -96,60 +97,60 @@ begin
 	   Port      : Serial_Communications.Serial_Port;
 
 	begin
-		Serial_Communications.Open
+		Serial_Communications.Open -- Opens the serial port
 		 (Port => Port,
 		  Name => Port_Name);
 
-		Serial_Communications.Set
-		 (Port      => Port,
+		Serial_Communications.Set -- Sets the details for the serial port
+		 (Port      => Port, 
 		  Rate      => Serial_Communications.B4800,
 		  Bits      => Serial_Communications.CS7,
 		  Stop_Bits => Serial_Communications.One,
 		  Parity    => Serial_Communications.Odd);
 
-		Serial_Communications.Write
+		Serial_Communications.Write -- Sends the inquiry
 		 (Port   => Port,
 		  Buffer => Enquiry);
 		  
 		  delay 0.1;
 		  
-		Serial_Communications.Write
+		Serial_Communications.Write -- Sets the pump's number
 		 (Port   => Port,
 		  Buffer => Numeration);
 
-		  delay 0.5;
+		  delay 0.1;
 		  
-		Serial_Communications.Write
+		Serial_Communications.Write -- Sets the control input (RPM, # revs, etc)
 		 (Port   => Port,
 		  Buffer => ControlInput);
 
 		  delay 0.1;
 		  
-		Serial_Communications.Write
+		Serial_Communications.Write -- Checks the pump's status
 		 (Port   => Port,
 		  Buffer => StatusCheck);
 		  
-		  delay 0.4;
+		  delay 0.1;
 
-		Serial_Communications.Read
+		Serial_Communications.Read -- Reads the pump's response to the StatusCheck
 		(Port => Port,
 		 Buffer => Buffer,
 		 Last => Last);
-		 
-		
 		
 		for I in 1 .. Last loop
-			Put (Stream_Element'Image(Buffer(I)));
+			Put (Stream_Element'Image(Buffer(I))); -- Prints the output from Read into the cmd prompt
 			New_Line;
 		end	loop;
 		
-		delay 10.0;
+		delay 5.0; -- Length the pump runs
 		
-		Serial_Communications.Write
+		Serial_Communications.Write -- Sends the pump the stop signal
 		 (Port   => Port,
 		  Buffer => StopSignal);
 
-		Serial_Communications.Close
+		delay 1.0;
+		
+		Serial_Communications.Close -- Closes the serial port
 		 (Port => Port);
 	end;
 end ctlr;
